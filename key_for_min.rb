1f712493412ae7339d9c3bbb4d4fd6342507a934
Charlie Knight <williamchknight@gmail.com>
2020-11-07 18:23:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.each do |key, value|
-    if value.min?
-      puts "#{key}"
-    end
+ name_array = name_hash.to_a 
+ holder_k = 0 
+ holder_v = 1000
+ name_array.each do |i|
+   if i[1] < holder_v
+     holder_v = i[1]
+     holder_k = i[0]
+   end
   end
+  holder_k
 end
